fe1bcd18bb27f35b9ee619deb1b68e6b17bcc96a
Ankit Jha <jhaa@us.ibm.com>
2020-02-20 12:10:04 -0600

    Adding db loggers

--- extremeanomaly/extremeanomalygenerator.py
+++ extremeanomaly/extremeanomalygenerator.py
@@ -39,9 +39,8 @@ class ExtremeAnomalyGenerator(BaseTransformer):
         schema = "BLUADMIN"
         logger.debug('Fire Database query')
         print('Fire Database query')
-        #df_result = db.read_agg(derived_metric_table_name,schema,{df_deviceid_col_name: 'count'},group_by=df_deviceid_col_name)
-        #logger.debug(df_result)
-
+        df_result = db.read_agg(derived_metric_table_name,schema,{df_deviceid_col_name: 'count'},group_by=df_deviceid_col_name)
+        logger.debug(df_result)
 
         logger.debug('Entity table name {}'.format(self.get_entity_type_param('name')))
         print('Entity table name {}'.format(self.get_entity_type_param('name')))
@@ -49,10 +48,10 @@ class ExtremeAnomalyGenerator(BaseTransformer):
         
         #logger.debug('Entity metricsTableName {}'.format(self.get_entity_type().get_attributes_dict()))
         logger.debug('Entity db {}'.format(self.get_db()))
-        logger.debug('Entity metadata {}'.format(self.get_db().entity_type_metadata[self.get_entity_type_param('name')]))
-        entity_type_metadata = self.get_db().entity_type_metadata[self.get_entity_type_param('name')]
-        logger.debug('Entity metadata type {}'.format(type(entity_type_metadata)))
-        logger.debug('Entity metadata data items {}'.format(entity_type_metadata._data_items))
+        logger.debug('Entity metadata {}'.format(self.get_db().entity_type_metadata.items()))
+        # entity_type_metadata = self.get_db().entity_type_metadata[self.get_entity_type_param('name')]
+        # logger.debug('Entity metadata type {}'.format(type(entity_type_metadata)))
+        # logger.debug('Entity metadata data items {}'.format(entity_type_metadata._data_items))
 
 
         #Divide the timeseries in (factor)number of splits.Each split will have one anomaly
