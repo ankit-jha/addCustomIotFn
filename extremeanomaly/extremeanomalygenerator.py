bb3725ad12d48467b2cbbe76cd17d5e625cd48d8
Ankit Jha <jhaa@us.ibm.com>
2020-02-20 14:12:21 -0600

    Adding entity loggers

--- extremeanomaly/extremeanomalygenerator.py
+++ extremeanomaly/extremeanomalygenerator.py
@@ -39,7 +39,7 @@ class ExtremeAnomalyGenerator(BaseTransformer):
         schema = "BLUADMIN"
         logger.debug('Fire Database query')
         print('Fire Database query')
-        df_result = db.read_agg(derived_metric_table_name,schema,{df_deviceid_col_name: 'count'},group_by=df_deviceid_col_name)
+        df_result = db.read_agg(derived_metric_table_name,schema,{df_deviceid_col_name: 'count'},group_by=[df_deviceid_col_name])
         logger.debug(df_result)
 
         logger.debug('Entity table name {}'.format(self.get_entity_type_param('name')))
