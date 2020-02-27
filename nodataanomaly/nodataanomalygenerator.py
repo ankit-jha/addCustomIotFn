14c70dae1369d0bcea370393007f57c0460e3931
Ankit Jha <jhaa@us.ibm.com>
2020-02-27 12:19:32 -0600

    Working

--- nodataanomaly/nodataanomalygenerator.py
+++ nodataanomaly/nodataanomalygenerator.py
@@ -25,24 +25,24 @@ class NoDataAnomalyGenerator(BaseTransformer):
     def execute(self, df):
 
         logger.debug('Dataframe shape {}'.format(df.shape))
-        
+
         entity_type = self.get_entity_type()
         logger.debug('isinstance of {}', type(entity_type))
         derived_metric_table_name = 'DM_'+self.get_entity_type_param('name')
         schema = entity_type._db_schema
 
-        #Store and initialize the counts by entity id 
+        #Store and initialize the counts by entity id
         db = self.get_db()
         query, table = db.query(derived_metric_table_name,schema,column_names='KEY',filters={'KEY':self.output_item})
         raw_dataframe = db.get_query_data(query)
-        logger.debug('Check for key column {} in derived metric table {}'.format(self.output_item,raw_dataframe.shape))
+        logger.debug('Check for key {} in derived metric table {}'.format(self.output_item,raw_dataframe.shape))
         key = '_'.join([derived_metric_table_name, self.output_item])
 
         if raw_dataframe is not None and raw_dataframe.empty:
             #Delete old counts if present
             db.cos_delete(key)
             logger.debug('Intialize count for first run')
-        
+
         counts_by_entity_id = db.cos_load(key,binary=True)
         if counts_by_entity_id is None:
             counts_by_entity_id = {}
@@ -63,29 +63,29 @@ class NoDataAnomalyGenerator(BaseTransformer):
             width = self.width
             if entity_grp_id in counts_by_entity_id:
                 count = counts_by_entity_id[entity_grp_id][0]
-                width = counts_by_entity_id[entity_grp_id][0]
+                width = counts_by_entity_id[entity_grp_id][1]
 
             mark_anomaly = False
             for grp_row_index in df_entity_grp.index:
                 count += 1
                 if count%self.factor == 0:
-                    #Start marking points as NaN
-                    mark_NaN = True
-                
+                    #Start marking points
+                    mark_anomaly = True
+
                 if mark_anomaly:
                     timeseries.iloc[grp_row_index] = np.NaN
                     width -= 1
                     logger.debug('Anomaly Index Value{}'.format(grp_row_index))
-                
+
                 if width==0:
-                    #End marking points as NaN
-                    mark_anomaly =False
+                    #End marking points
+                    mark_anomaly = False
                     width = self.width
-                
+
                 counts_by_entity_id[entity_grp_id] = (count,width)
 
         logger.debug('Final Grp Counts {}'.format(counts_by_entity_id))
-        
+
         #Save the group counts to cos
         db.cos_save(counts_by_entity_id,key,binary=True)
 
@@ -102,15 +102,17 @@ class NoDataAnomalyGenerator(BaseTransformer):
                                               ))
 
         inputs.append(UISingle(
-                name='width',
+                name='factor',
                 datatype=int,
-                description='Width of the anomaly created- default 100'
+                description='Frequency of anomaly e.g. A value of 3 will create anomaly every 3 datapoints',
+                default=10
                                               ))
 
         inputs.append(UISingle(
-                name='factor',
+                name='width',
                 datatype=int,
-                description='No. of nodata anomalies to be created'
+                description='Width of the anomaly created',
+                default=5
                                               ))
 
         outputs = []
