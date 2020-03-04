bf86084df2aa58cede9d7977e10f202f95e48c7b
Ankit Jha <jhaa@us.ibm.com>
2020-03-03 22:41:58 -0600

    Close to working

--- flatlineanomalyaj/flatlineanomalygeneratoraj.py
+++ flatlineanomalyaj/flatlineanomalygeneratoraj.py
@@ -42,7 +42,7 @@ class FlatlineAnomalyGenerator(BaseTransformer):
             db.model_store.delete_model(key)
             logger.debug('Intialize count for first run')
 
-        counts_by_entity_id = db.model_store.retrieve_model(key)
+        counts_by_entity_id = db.model_store.retrieve_model(key,deserialize=False)
         if counts_by_entity_id is None:
             counts_by_entity_id = {}
         logger.debug('Initial Grp Counts {}'.format(counts_by_entity_id))
